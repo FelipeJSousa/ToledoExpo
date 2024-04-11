23c39293bdd9fba551fc0fe252abd7df28ade2a4
Felipe Sousa <felipejosecsousa@gmail.com>
2024-04-11 20:49:55 -0300

    feat(controller): add AtendenteController

---
+++ src/ToledoExpo.Services.API/Controllers/AtendenteController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToledoExpo.Services.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AtendenteController : Controller
+{
+    public AtendenteController()
+    {
+        
+    }
+    
+    /// <summary>
+    ///     Criar novo atendente.
+    /// </summary>
+    /// <returns></returns>
+    [Produces("application/json")]
+    [Consumes("application/json")]
+    [HttpPost]
+    public IActionResult NovoAtendente()
+    {
+        return Ok();
+    }
+}
---
+++ src/ToledoExpo.Services.API/ViewModels/ApiRequestNovoAtendente.cs
@@ -0,0 +1,9 @@
+namespace ToledoExpo.Services.API.ViewModels;
+
+public class ApiRequestNovoAtendente
+{
+    public string Nome { get; set; }
+    
+    public long Estabelecimento { get; set; }
+    
+}
