a0c940860f6a73a701a25d7ca20ef8c4e46243ba
Felipe Sousa <felipejosecsousa@gmail.com>
2024-04-11 20:31:35 -0300

    feat(tests): Domain - add estabelecimentoTest

---
+++ tests/ToledoExpo.Services.UnitTestDomain/Entities/EstabelecimentoTest.cs
@@ -0,0 +1,30 @@
+using ToledoExpo.Services.Domain.Entities;
+
+namespace ToledoExpo.Services.UnitTestDomain;
+
+public class EstabelecimentoTest
+{
+    [Fact(DisplayName = "Deve instanciar um estabelecimento vazio")]
+    public void DeveInstanciarEstabelecimentoVazio()
+    {
+        var obj = new Estabelecimento();
+        
+        Assert.Equal(0, obj.Id);
+        Assert.Null(obj.Nome);
+    }
+    
+    [Fact(DisplayName = "Deve instanciar um estabelecimento valido")]
+    public void DeveInstanciarEstabelecimentoValido()
+    {
+        var obj = new Estabelecimento(1, "Estabelecimento 1", "Descrição do estabelecimento 1", "Felipe Sousa");
+        
+        Assert.NotNull(obj);
+        Assert.Equal(1, obj.Id);
+        Assert.Equal("Estabelecimento 1", obj.Nome);
+        Assert.Equal("Descrição do estabelecimento 1", obj.Descricao);
+        Assert.Equal("Felipe Sousa", obj.Proprietario);
+        Assert.True(obj.DataCricao < DateTime.Now);
+        Assert.True(obj.Ativo);
+        
+    }
+}
