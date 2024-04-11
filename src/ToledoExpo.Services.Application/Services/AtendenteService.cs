06f85e9406344b6a53ba7cb57529553a9eb7f4a8
Felipe Sousa <felipejosecsousa@gmail.com>
2024-04-11 20:45:13 -0300

    feat(application): add AtendenteService

---
+++ src/ToledoExpo.Services.Application/Services/AtendenteService.cs
@@ -0,0 +1,15 @@
+using ToledoExpo.Services.Domain.Entities;
+using ToledoExpo.Services.Domain.Interfaces;
+using ToledoExpo.Services.Domain.Interfaces.Services;
+
+namespace ToledoExpo.Services.Application.Services;
+
+public class AtendenteService : IAtendenteService
+{
+    private IRepository<Atendente> _Repo;
+    
+    public AtendenteService(IRepository<Atendente> repo)
+    {
+        _Repo = repo;
+    }
+}
