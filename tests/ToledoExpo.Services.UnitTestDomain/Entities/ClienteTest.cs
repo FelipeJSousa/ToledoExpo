3d41a3ef34dd7a2e5e6a78920777ad7f1d686750
Gabriel Lanza <gabriel-lanza21@outlook.com>
2024-04-13 17:55:55 -0300

    Feat(Test) - Add Cliente Test Initial

---
+++ tests/ToledoExpo.Services.UnitTestDomain/Entities/ClienteTest.cs
@@ -0,0 +1,32 @@
+﻿using ToledoExpo.Services.Domain.Entities;
+//Resharper disable all
+
+namespace ToledoExpo.Services.UnitTestDomain.Entities;
+public class ClienteTest
+{
+
+    [Fact(DisplayName = "Deve instanciar um cliente vazio")]
+    public void DeveInstanciarClienteVazio()
+    {
+        var obj = new Cliente();
+
+        Assert.Equal(0, obj.Id);
+        Assert.Null(obj.Nome);
+    }
+
+    [Fact(DisplayName = "Deve instanciar um cliente valido")]
+    public void DeveInstanciarClienteValido()
+    {
+        var nome = "Gabriel Lanza";
+        var capacidadeCognitiva = 2.50;
+        var velocidadeMovimento = 5;
+         
+        var obj = new Cliente(nome, velocidadeMovimento, capacidadeCognitiva);
+
+        Assert.NotNull(obj);
+        Assert.Equal(nome, obj.Nome);
+        Assert.Equal(capacidadeCognitiva, obj.CapacidadeCognitiva);
+        Assert.Equal(velocidadeMovimento, obj.VelocidadeMovimento);
+    }
+
+}
