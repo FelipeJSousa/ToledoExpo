67ff69e92094e8757b8ad83721aff788fc121032
Gabriel Lanza <gabriel-lanza21@outlook.com>
2024-04-13 17:53:32 -0300

    Feat(Configurations) - Add Scoped Repository

--- src/ToledoExpo.Services.API/Configurations/IoC/RegisterApplicationServices.cs
+++ src/ToledoExpo.Services.API/Configurations/IoC/RegisterApplicationServices.cs
@@ -1,5 +1,7 @@
 using ToledoExpo.Services.Application.Services;
+using ToledoExpo.Services.Domain.Interfaces;
 using ToledoExpo.Services.Domain.Interfaces.Services;
+using ToledoExpo.Services.Infraestructure.Data.Repositories;
 
 namespace ToledoExpo.Services.API.Configurations.IoC;
 
@@ -7,8 +9,10 @@ public static class RegisterApplicationServices
 {
     public static IServiceCollection RegisterServices(this IServiceCollection services)
     {
+        services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
         services.AddScoped<IAtendenteService, AtendenteService>();
-        
+
         return services;
     }
 }
