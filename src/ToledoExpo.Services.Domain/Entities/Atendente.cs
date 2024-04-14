5630e7285a557996ab15d6116c1532f1093f63ff
Felipe Sousa <felipejosecsousa@gmail.com>
2024-04-14 13:38:02 -0300

    feat(domain): add Atendente

--- src/ToledoExpo.Services.Domain/Entities/Atendente.cs
+++ src/ToledoExpo.Services.Domain/Entities/Atendente.cs
@@ -1,12 +1,15 @@
+using ToledoExpo.Services.Core.Entities;
+
 namespace ToledoExpo.Services.Domain.Entities;
 
 public class Atendente : Entity
 {
     public string Nome { get; set; }
-    
+
     public long Estabelecimento { get; set; }
-    
+
     public Estabelecimento EstabelecimentoObj { get; set; }
 
     public double TempoAtendimentoMinimo { get; set; }
+    
 }
--- tests/ToledoExpo.Services.UnitTest.Domain/Entities/AtendenteTest.cs
+++ tests/ToledoExpo.Services.UnitTest.Domain/Entities/AtendenteTest.cs
@@ -13,17 +13,4 @@ public class AtendenteTest
         Assert.Equal(0, obj.Id);
     }
 
-    [Fact]
-    public void DeveIniciarAtendimento()
-    {
-        // arrange
-        var obj = new Atendente();
-
-        // act
-        obj.IniciarAtendimento("felipe");
-        
-        // assert
-        Assert.NotEqual(0, obj.Id);
-        Assert.Equal("felipe", obj.Nome);
-    }
 }
