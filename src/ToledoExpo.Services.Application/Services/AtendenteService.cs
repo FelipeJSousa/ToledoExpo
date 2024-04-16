0c091a2bf806588221a132c241cdbd1acd928455
Gabriel Lanza <gabriel-lanza21@outlook.com>
2024-04-16 00:02:27 -0300

    Feat(Services) - Add AtendenteService + EstabelecimentoService

--- src/ToledoExpo.Services.Application/Services/AtendenteService.cs
+++ src/ToledoExpo.Services.Application/Services/AtendenteService.cs
@@ -1,4 +1,9 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using LinqKit;
 using ToledoExpo.Services.Core.Interfaces.Repositories;
 using ToledoExpo.Services.Core.Services;
 using ToledoExpo.Services.Domain.Entities;
@@ -11,4 +16,47 @@ public class AtendenteService : ServiceBase<Atendente>, IAtendenteService
     public AtendenteService(IServiceProvider serviceProvider, IRepository<Atendente> repoBase) : base(serviceProvider, repoBase)
     {
     }
+
+    public async Task<IEnumerable<Atendente>> Listar(Atendente filter = null)
+    {
+        if (filter is null)
+            return await GetList();
+        
+        Expression<Func<Atendente, bool>> predicate = PredicateBuilder.New<Atendente>(true);
+
+        if (!string.IsNullOrEmpty(filter.Nome))
+            predicate = predicate.And(x => x.Nome.Contains(filter.Nome));
+
+        if (filter.Estabelecimento > 0)
+            predicate = predicate.And(x => x.Estabelecimento == filter.Estabelecimento);
+
+        return await GetList(predicate);
+    }
+
+    public async Task<Atendente> Obter(Atendente filter = null)
+    {
+        Expression<Func<Atendente, bool>> predicate = PredicateBuilder.New<Atendente>(true);
+
+        if (filter?.Id > 0)
+            predicate = predicate.And(x => x.Id == filter.Id);
+
+        if (!string.IsNullOrEmpty(filter?.Nome))
+            predicate = predicate.And(x => x.Nome.Contains(filter.Nome));
+
+        if (filter?.Estabelecimento > 0)
+            predicate = predicate.And(x => x.Estabelecimento == filter.Estabelecimento);
+
+        return await GetSingle(predicate);
+    }
+
+    public async Task<Atendente> Cadastrar(Atendente obj)
+    {
+        if (obj is null)
+            return default;
+
+        obj.Novo();
+
+        return await Save(obj);
+    }
+
 }
--- src/ToledoExpo.Services.Application/Services/EstabelecimentoService.cs
+++ src/ToledoExpo.Services.Application/Services/EstabelecimentoService.cs
@@ -1,4 +1,8 @@
+using LinqKit;
 using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
 using ToledoExpo.Services.Core.Interfaces.Repositories;
 using ToledoExpo.Services.Core.Services;
 using ToledoExpo.Services.Domain.Entities;
@@ -10,5 +14,53 @@ public class EstabelecimentoService : ServiceBase<Estabelecimento>, IEstabelecim
 {
     public EstabelecimentoService(IServiceProvider serviceProvider, IRepository<Estabelecimento> repoBase) : base(serviceProvider, repoBase)
     {
+    }
+
+    public async Task<IEnumerable<Estabelecimento>> Listar(Estabelecimento filter = null)
+    {
+        if (filter is null)
+            return await GetList();
+
+        Expression<Func<Estabelecimento, bool>> predicate = PredicateBuilder.New<Estabelecimento>(true);
+
+        if (!string.IsNullOrEmpty(filter.Nome))
+            predicate = predicate.And(x => x.Nome.Contains(filter.Nome));
+
+        if (!string.IsNullOrEmpty(filter.Proprietario))
+            predicate = predicate.And(x => x.Proprietario.Contains(filter.Proprietario));
+
+        if (!string.IsNullOrEmpty(filter.Descricao))
+            predicate = predicate.And(x => x.Descricao.Contains(filter.Descricao));
+
+        return await GetList(predicate);
+    }
+
+    public async Task<Estabelecimento> Obter(Estabelecimento filter = null)
+    {
+        Expression<Func<Estabelecimento, bool>> predicate = PredicateBuilder.New<Estabelecimento>(true);
+
+        if (filter?.Id > 0)
+            predicate = predicate.And(x => x.Id == filter.Id);
+
+        if (!string.IsNullOrEmpty(filter?.Nome))
+            predicate = predicate.And(x => x.Nome.Contains(filter.Nome));
+
+        if (!string.IsNullOrEmpty(filter?.Proprietario))
+            predicate = predicate.And(x => x.Proprietario.Contains(filter.Proprietario));
+
+        if (!string.IsNullOrEmpty(filter?.Descricao))
+            predicate = predicate.And(x => x.Descricao.Contains(filter.Descricao));
+
+        return await GetSingle(predicate);
+    }
+
+    public async Task<Estabelecimento> Cadastrar(Estabelecimento obj)
+    {
+        if (obj is null)
+            return default;
+
+        obj.Novo();
+
+        return await Save(obj);
     }
 }
